4f3c54f46c5f4a92ee41de4806aae71c690ea6ef
Dimitri Mateus <Dimitriestudo@gmail.com>
2024-04-03 11:19:29 -0300

    Primeira versão

---
+++ calculadora.py
@@ -0,0 +1,79 @@
+import time
+import math 
+
+continuar = "S"
+while continuar == "S" or continuar == "s" or continuar == "SIM" or continuar == "sim":
+    print("-"*50)
+    print("         Qual operação você deseja fazer? ")
+    print("-"*50)
+    operacao = str(input(" Adição \n Subtração \n Multiplicação \n Divisão \n Potenciação \n Radiciação \n Logaritimação \n "))
+    while operacao != "Adição" and operacao != "Subtração" and operacao != "Multiplicação" and operacao != "Divisão" and operacao != "Potenciação" and operacao != "Radiciação" and operacao != "Logaritimação":
+        print("-"*100)
+        print("    Essa operação é desconhecida, por favor escreva da forma da mesma forma que foi perguntado. ")
+        print("-"*100)
+        time.sleep(1.25)
+        operacao = str(input(" Adição \n Subtração \n Multiplicação \n Divisão \n Potenciação \n Radiciação \n Logaritimação \n "))
+    print("-"*50)
+    print(f"        A operação escolhida foi {operacao}")
+    print("-"*50)
+    time.sleep(1)
+    lista_numeros = []
+    
+    #Separação entre as operações em que o usuario pode dar mais que dois valores
+    if operacao == "Potenciação":
+        vezes = 2
+        for c in range(1,vezes +1):
+            num = int(input(f"Digite o {c}° número: "))
+            lista_numeros.append(num)
+    elif operacao == "Adição" or operacao == "Subtração" or operacao == "Multiplicação" or operacao == "Divisão":
+        vezes = int(input("Com quantos números voce deseja fazer esta operação? "))
+        for c in range(1,vezes +1):
+            num = int(input(f"Digite o {c}° número: "))
+            lista_numeros.append(num)
+    resultado = 0
+    
+    
+    #Calculos 
+    if operacao == "Adição":
+        for item in lista_numeros:
+            resultado += item
+    elif operacao == "Subtração":
+        resultado = lista_numeros[0]
+        for item in lista_numeros[1:]:
+            resultado -= item
+    elif operacao == "Multiplicação":
+        resultado = lista_numeros[0]
+        for item in lista_numeros[1:]:
+            resultado *= item
+    elif operacao == "Divisão":
+        resultado = lista_numeros[0]  
+        for item in lista_numeros[1:]:  
+            resultado /= item
+    elif operacao == "Potenciação":
+        resultado = lista_numeros[0]
+        for item in lista_numeros[1:]:
+            resultado **= item
+    print(f"O resultado é {resultado}")
+    time.sleep(1)
+    continuar = str(input("Você quer fazer mais alguma operação?"))
+    time.sleep(1)
+time.sleep(1)
+print("Calculadora fechada!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
