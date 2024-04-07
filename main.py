c4782be5f2dc525a437faf284b8cef475db11235
Dimitri Mateus <Dimitriestudo@gmail.com>
2024-04-07 11:25:35 -0300

    Mudanças na interface

--- main.py
+++ main.py
@@ -3,39 +3,37 @@ import math
 
 continuar = "S"
 while continuar == "S" or continuar == "s" or continuar == "SIM" or continuar == "sim":
-    print("-"*50)
-    print("         Qual operação você deseja fazer? ")
-    print("-"*50)
+    print(" | Qual operação você quer fazer?  |")
     #Perguntando a operação
-    operacao = str(input(" Adição \n Subtração \n Multiplicação \n Divisão \n Potenciação \n Radiciação \n Logaritimação \n "))
+    operacao = str(input(" | Adição         | Subtração      |\n | Multiplicação  | Divisão        |\n | Potenciação    | Radiciação     |\n | Logaritimação  | "))
     while operacao != "Adição" and operacao != "Subtração" and operacao != "Multiplicação" and operacao != "Divisão" and operacao != "Potenciação" and operacao != "Radiciação" and operacao != "Logaritimação":
-        print("-"*100)
-        print("    Essa operação é desconhecida, por favor escreva da forma da mesma forma que foi perguntado. ")
-        print("-"*100)
+        print("\n Essa operação é desconhecida, por favor escreva da forma da mesma forma que foi perguntado.")
         time.sleep(1.25)
-        operacao = str(input(" Adição \n Subtração \n Multiplicação \n Divisão \n Potenciação \n Radiciação \n Logaritimação \n "))
-    print("-"*50)
-    print(f"        A operação escolhida foi {operacao}")
-    print("-"*50)
+        operacao = str(input("\n | Adição         | Subtração      |\n | Multiplicação  | Divisão        |\n | Potenciação    | Radiciação     |\n | Logaritimação  | "))
+    print(f" |  A operação escolhida foi {operacao}  |")
     time.sleep(1)
     lista_numeros = []
     #Separação entre as operações em que o usuario pode dar mais que dois valores
-    
     match operacao:
         case "Potenciação":
             vezes = 2
             for c in range(1,vezes +1):
-                num = int(input(f"Digite o {c}° número: "))
+                num = float(input(f"Digite o {c}° número: "))
                 lista_numeros.append(num)
         case "Radiciação":
-            num = int(input("Digite o número: "))
+            num = float(input("Digite o número: "))
         case "Logaritimação":
-            num = int(input("Digite o número: "))
-            base = int(input("Qual a base? "))
+                num = float(input("Digite o número: "))
+                base = float(input("Qual a base? "))        
+                while base == 0:
+                    print(" |  A base não pode ser 0  |")
+                    time.sleep(0.25)
+                    num = float(input("Digite o número: "))
+                    base = float(input("Qual a base? ")) 
         case _:
-            vezes = int(input("Com quantos números voce deseja fazer esta operação? "))
+            vezes = int(input(" |  Com quantos números voce deseja fazer esta operação? "))
             for c in range(1,vezes +1):
-                num = int(input(f"Digite o {c}° número: "))
+                num = float(input(f"Digite o {c}° número: "))
                 lista_numeros.append(num)
     resultado = 0 
     #Calculos 
@@ -62,11 +60,10 @@ while continuar == "S" or continuar == "s" or continuar == "SIM" or continuar ==
         case "Radiciação":
             resultado = round(math.sqrt(num),3)
         case "Logaritimação":
-            resultado = math.log(num, base)
-            
+            resultado = round(math.log(num, base)) 
     print(f"O resultado é {resultado}")
-    time.sleep(1)
-    continuar = str(input("Você quer fazer mais alguma operação?"))
     time.sleep(0.25)
-time.sleep(1)
+    continuar = str(input("|  Você quer fazer mais alguma operação? "))
+    time.sleep(0.25)
+time.sleep(0.5)
 print("Calculadora fechada!")
